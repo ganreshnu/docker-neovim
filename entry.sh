60ea03c3e537e28e45db86bf2559de593941794a
Ganreshnu <ganreshnu@gmail.com>
2022-07-13 11:37:48 -0500

    updated the entry script

--- entry.sh
+++ entry.sh
@@ -1,17 +1,31 @@
 #!/bin/bash -il
 
 function showhelp() {
-	cat << 'EOD'
-This is the help.
+	cat << EOD
+Usage: nvim [OPTIONS] COMMAND
+
+A docker container for Neovim.
+
+Options:
+  --update	Updates the configuration
+  --git-name	Git configuration user.name
+  --get-email	Git configuration user.email
+  --help	Show this message
+
+In order to use gpg, you must either build an additional container or bind mount
+the correct sockets.
+
+# with the host gpg-agent
+alias nvim='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v "\$PWD":"\$PWD" -w "\$PWD" -v "\$(gpgconf --list-dir agent-extra-socket)":"$(gpgconf --list-dir agent-socket)" -v "\$(gpgconf --list-dir agent-ssh-socket)":"$(gpgconf --list-dir agent-ssh-socket)" neovim --git-name "\$(git config user.name)" --git-email "\$(git config user.email)" nvim'
+
+# with the container gpg-agent
+alias nvim='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v "\$PWD":"\$PWD" -w "\$PWD" neovim --git-name "\$(git config user.name)" --git-email "\$(git config user.email)" nvim'
+
+To build the key into a container, clone https://github.com/ganreshnu/docker-neovim.git and run:
+
+. .env && build --help
 EOD
 }
-
-for i in ${XDG_CONFIG_HOME}/*; do
-	git -C $i pull --quiet
-done
-git -C ${HOME}/.ssh pull --quiet
-
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 if [ $# -eq 0 ]; then
 	showhelp
@@ -35,9 +49,17 @@ do
 				git config --global user.email "$2"
 				shift 2
 				;;
+			--update )
+				for i in ${XDG_CONFIG_HOME}/*; do
+					git -C $i pull --quiet
+				done
+				git -C ${HOME}/.ssh pull --quiet
+				nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+				shift
+				;;
 			* )
-				echo "unknown option $1"
-				wanthelp=1
+				>&2 echo "unknown option: $1"
+				wanthelp=2
 				shift
 				;;
 		esac
@@ -46,15 +68,7 @@ do
 	fi
 done
 
-if [ $wanthelp -eq 1 ]; then
-	showhelp
-	exit
-fi
-
-#
-# Add these lines to your .bashrc
-#
-#alias nvim='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v "\$PWD":"\$PWD" -w "\$PWD" -v "\$(gpgconf --list-dir agent-extra-socket)":"$(gpgconf --list-dir agent-socket)" -v "\$(gpgconf --list-dir agent-ssh-socket)":"$(gpgconf --list-dir agent-ssh-socket)" neovim nvim'
-#alias nvim='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v "\$PWD":"\$PWD" -w "\$PWD" neovim bash'
+[ $wanthelp -eq 1 ] && showhelp && exit
+[ $wanthelp -eq 2 ] && showhelp && exit 1
 
 exec $@
